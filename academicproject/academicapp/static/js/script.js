ad80fde9177a0b49d6e6ef9a89d75ea3767553fc
Rich Anthonios <rich.anthonios@student.president.ac.id>
2025-07-21 19:59:54 +0700

    new dashboard

--- academicproject/academicapp/static/js/script.js
+++ academicproject/academicapp/static/js/script.js
@@ -101,31 +101,45 @@ function toggleDropdown() {
 
 document.addEventListener('DOMContentLoaded', function () {
   const ctx = document.getElementById('myChart').getContext('2d');
+  
+  const gradient = ctx.createLinearGradient(0, 0, 0, 250);  
+  gradient.addColorStop(0, 'rgba(54, 162, 235, 0.5)');
+  gradient.addColorStop(1, 'rgba(54, 162, 235, 0)');
 
   const myChart = new Chart(ctx, {
     type: 'line',
     data: {
       labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'August'],
       datasets: [{
-        data: [100, 150, 60, 140, 90, 200, 130, 160],
+        data: [100, 150, 80, 140, 90, 170, 130, 160],
         fill: true,
         borderColor: 'rgba(54, 162, 235, 1)',
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        backgroundColor: gradient,
         tension: 0.4,
-        pointRadius: 4,
-        pointHoverRadius: 6
+        pointRadius: 0,
+        pointHoverRadius: 8,
+        pointHitRadius: 20
       }]
     },
     options: {
       responsive: true,
-      maintainAspectRatio: false,
+      maintainAspectRatio: true,
       plugins: {
         legend: {
           display: false 
         },
         title: {
           display: true,
-          text: 'Active Lecturer'
+          text: 'Active Lecturer',
+          align: 'start',
+          padding: {
+            bottom: 30,
+          },
+          font: {
+            size: 18,
+            weight: 'bold',
+            family: 'poppins'
+          }
         }
       },
       scales: {
@@ -134,15 +148,20 @@ document.addEventListener('DOMContentLoaded', function () {
             display: true
           },
           grid: {
-            display: true
+            display: false,
           }
         },
         y: {
+          min: 60,
           ticks: {
-            display: true
+            display: false,
           },
           grid: {
-            display: true 
+            display: true,
+          },
+          border: {
+            dash: [2,4],
+            display: false
           }
         }
       }
